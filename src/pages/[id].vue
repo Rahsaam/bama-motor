a35748ecd938194d1111871d3baa231067a1a543
Hesam <hrahmaniii20@gmail.com>
2024-12-14 19:25:54 +0330

    feat: add single products page

---
+++ src/pages/[id].vue
@@ -0,0 +1,34 @@
+<script setup>
+const route = useRoute()
+
+const { getProductById } = useProductFilters()
+const productId = Number(route.params.id)
+const product = getProductById(productId)
+</script>
+
+<template>
+  <div>
+    <ProductDetailModal :product="product" />
+  </div>
+</template>
+
+<style scoped>
+.page-container {
+  max-width: 800px;
+  margin: 0 auto;
+}
+
+/* انیمیشن مودال */
+.modal-enter-active,
+.modal-leave-active {
+  transition:
+    opacity 0.5s,
+    transform 0.3s;
+}
+
+.modal-enter,
+.modal-leave-to {
+  opacity: 0;
+  transform: scale(0.95);
+}
+</style>
