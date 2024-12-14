2ec5b16758af4e76ec445738dd97ceca6b55b289
Hesam <hrahmaniii20@gmail.com>
2024-12-14 14:32:55 +0330

    feat: add price filter

--- src/components/FilterFields.vue
+++ src/components/FilterFields.vue
@@ -2,13 +2,12 @@
   <filter-container>
     <ui-dialog-filter />
     <Ui-seller-filter />
+    <div dir="ltr">
+      <Ui-Price-Filter-Dual-input :min="0" :max="5000000000" />
+    </div>
   </filter-container>
 </template>
 
-<script setup>
+<script setup></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
